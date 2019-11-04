46e8faf5f258b911ed179eaa317c38ca093c5c40
Roberto Delgado <robertodelgado@Robertos-MacBook-Air.local>
2019-11-03 15:51:04 -1000

    Added mouseenter & mouseleave event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+const navItems = document.querySelectorAll('.nav-link');
+
+navItems.forEach((item) => {
+    item.addEventListener("mouseenter", () => {
+        item.style.fontFamily = "Impact";
+    })
+})
+
+navItems.forEach((item) => {
+    item.addEventListener("mouseleave", () => {
+        item.style.fontFamily = "Times New Roman";
+    })
+})
+
